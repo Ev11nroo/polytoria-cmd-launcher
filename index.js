9334bfb0f68397191a85e5f7b807d0bf5629d1fd
roo <146498770+Ev11nroo@users.noreply.github.com>
2026-02-15 21:11:08 -0500

    add launch logic + some other changes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { cookie, command, placeId } = require("./config.json");
+const { cookie, placeId } = require("./config.json");
+let { command } = require("./config.json");
+const { execSync } = require("child_process");
 
 async function getLaunchToken() {
     const options = {
@@ -16,9 +18,36 @@ async function getLaunchToken() {
     const data = (await response).json();
 
     if (data.success !== true) {
-        console.log(`endpoint failed (${(await response).status})`);
+        console.error(`endpoint failed (${(await response).status})`);
         return 1;
     }
 
     return data.token;
 }
+
+(async () => {
+    const token = await getLaunchToken();
+    const uri = `polytoria://client/${token}`;
+
+    if (token == null || token == 1) {
+        console.error("failed to launch");
+        return 1;
+    }
+
+    if (command == null) {
+        console.log(`URI: ${uri}`);
+        return 0;
+    }
+
+    if (command.includes("%URI")) {
+        command = command.replace("%URI", uri);
+        console.log(`running ${command}`);
+        execSync(command);
+
+        return 0;
+    }
+
+    console.log(`running ${command} "${uri}"`);
+    execSync(`${command} "${uri}"`);
+    return 0;
+})();
