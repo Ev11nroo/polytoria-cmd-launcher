b05028d0b3b76f5ceaf9cb46727d80ee4a5f50de
roo <146498770+Ev11nroo@users.noreply.github.com>
2026-02-15 21:03:50 -0500

    add logic to get launch token

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const { cookie, command, placeId } = require("./config.json");
 
+async function getLaunchToken() {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Cookie: `${cookie}`
+        },
+        body: JSON.stringify({
+            placeID: placeId
+        })
+    };
+
+    const response = await fetch("https://polytoria.com/api/places/join", options);
+    const data = (await response).json();
+
+    if (data.success !== true) {
+        console.log(`endpoint failed (${(await response).status})`);
+        return 1;
+    }
+
+    return data.token;
+}
